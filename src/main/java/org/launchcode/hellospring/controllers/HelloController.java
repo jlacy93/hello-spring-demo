a1cd69e7159d7427c18eee9dd87d2f41f01b4ae3
jlacy <josephinalacy@gmail.com>
2023-07-18 21:17:43 -0500

    query-parameters

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,2 +1,29 @@
-package org.launchcode.hellospring.controllers;public class HelloController {
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    //Handles requests at /hello
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello(){
+//        return "Hello, Spring";
+//    }
+
+    @GetMapping("goodbye")
+    @ResponseBody
+    public String goodbye(){
+        return "Goodbye, Spring";
+    }
+
+    //Handles request of the form /hello?name=launchcode
+    @GetMapping("hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name){
+        return "Hello, " + name + "!";
+    }
 }
